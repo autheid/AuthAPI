dfe489084cec27f79192973fda757f24b147ef23
Pavel Kokolemin <pavelkok@gmail.com>
2019-03-03 17:24:56 +0300

    Add C++ encrypt utils

---
+++ utils/cpp/autheid_utils.cpp
@@ -0,0 +1,130 @@
+#include "autheid_utils.h"
+
+#include <botan/auto_rng.h>
+#include <botan/bigint.h>
+#include <botan/ecdh.h>
+#include "botan/ecies.h"
+#include "botan/ecdsa.h"
+
+namespace autheid
+{
+
+namespace  {
+
+const Botan::EC_Group kDomain("secp256k1");
+
+const auto kSignAlg = "EMSA1(SHA-256)";
+
+// Use stream cipher and compressed point to get smaller encrypted size
+const Botan::ECIES_System_Params kEciesParams(kDomain,
+   "KDF2(SHA-256)", "ChaCha(20)", 32, "HMAC(SHA-256)", 20,
+   Botan::PointGFp::COMPRESSED, Botan::ECIES_Flags::NONE);
+
+} // namespace
+
+PrivateKey generatePrivateKey()
+{
+   Botan::AutoSeeded_RNG rnd;
+   return rnd.random_vec(kPrivateKeySize);
+}
+
+PublicKey getPublicKey(const PrivateKey &privateKey)
+{
+   try {
+      Botan::AutoSeeded_RNG rnd;
+
+      Botan::BigInt privateKeyValue;
+      privateKeyValue.binary_decode(privateKey);
+      Botan::ECDH_PrivateKey privateKeyEC(rnd, kDomain, privateKeyValue);
+      privateKeyValue.clear();
+
+      Bytes publicKey = privateKeyEC.public_point().encode(Botan::PointGFp::COMPRESSED);
+      if (publicKey.size() != kPublicKeySize) {
+         return {};
+      }
+
+      return publicKey;
+   } catch (...){
+      return {};
+   }
+}
+
+Bytes encryptData(const void *data, size_t dataSize, const PublicKey &publicKey)
+{
+   try {
+      Botan::AutoSeeded_RNG rng;
+
+      auto publicKeyDecoded = Botan::OS2ECP(publicKey.data(), publicKey.size(), kDomain.get_curve());
+
+      Botan::ECIES_Encryptor encrypt(rng, kEciesParams);
+
+      encrypt.set_other_key(publicKeyDecoded);
+
+      Bytes result = encrypt.encrypt(static_cast<const uint8_t*>(data), dataSize, rng);
+
+      return result;
+   } catch (...) {
+      return {};
+   }
+}
+
+SecureBytes decryptData(const void *data, size_t dataSize, const PrivateKey &privateKey)
+{
+   try {
+      Botan::AutoSeeded_RNG rng;
+
+      Botan::BigInt privateKeyValue;
+      privateKeyValue.binary_decode(privateKey);
+      Botan::ECDH_PrivateKey privateKeyDecoded(rng, kDomain, privateKeyValue);
+      privateKeyValue.clear();
+
+      Botan::ECIES_Decryptor decryptor(privateKeyDecoded, kEciesParams, rng);
+
+      auto result = decryptor.decrypt(static_cast<const uint8_t*>(data), dataSize);
+      return result;
+
+   } catch (...){
+      return {};
+   }
+}
+
+Bytes signData(const void *data, size_t dataSize, const PrivateKey &privateKey)
+{
+   try {
+      Botan::AutoSeeded_RNG rng;
+
+      Botan::BigInt privateKeyValue;
+      privateKeyValue.binary_decode(privateKey);
+      Botan::ECDSA_PrivateKey privateKeyDecoded(rng, kDomain, privateKeyValue);
+      // Clear data for security
+      privateKeyValue.clear();
+
+      Botan::PK_Signer signer(privateKeyDecoded, rng, kSignAlg);
+      signer.update(static_cast<const uint8_t*>(data), dataSize);
+      Bytes signature = signer.signature(rng);
+
+      return signature;
+   } catch (...){
+      return {};
+   }
+}
+
+bool verifyData(const void *data, size_t dataSize, const void *sign, size_t signSize
+   , const PublicKey &publicKey)
+{
+   try {
+      auto publicKeyValue = Botan::OS2ECP(publicKey.data(), publicKey.size(), kDomain.get_curve());
+
+      Botan::ECDSA_PublicKey publicKeyDecoded(kDomain, publicKeyValue);
+
+      Botan::PK_Verifier verifier(publicKeyDecoded, kSignAlg);
+      verifier.update(static_cast<const uint8_t *>(data), dataSize);
+      bool result = verifier.check_signature(static_cast<const uint8_t *>(sign), signSize);
+
+      return result;
+   } catch (...){
+      return false;
+   }
+}
+
+} // namespace autheid
---
+++ utils/cpp/autheid_utils.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <cstdint>
+#include <vector>
+#include <string>
+#include <botan/secmem.h>
+
+namespace autheid
+{
+
+// secp256k1 secret key
+constexpr size_t kPrivateKeySize = 32;
+
+// secp256k1 EC point (compressed)
+constexpr size_t kPublicKeySize = 33;
+
+using Bytes = std::vector<uint8_t>;
+using SecureBytes = Botan::secure_vector<uint8_t>;
+
+using PublicKey = Bytes;
+using PrivateKey = SecureBytes;
+
+PrivateKey generatePrivateKey();
+PublicKey getPublicKey(const PrivateKey &privateKey);
+
+Bytes encryptData(const void *data, size_t dataSize, const PublicKey &publicKey);
+
+// Returns empty data if decrypt failed
+SecureBytes decryptData(const void *data, size_t dataSize, const PrivateKey &privateKey);
+
+Bytes signData(const void *data, size_t dataSize, const PrivateKey &privateKey);
+
+bool verifyData(const void *data, size_t dataSize, const void *sign, size_t signSize
+   , const PublicKey &publicKey);
+
+} // namespace autheid
